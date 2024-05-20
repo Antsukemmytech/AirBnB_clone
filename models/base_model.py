2f3bb6d3ef3d2aed04c64790ee15d215b94e0a65
antsukemmytech <antsukemmytech@gmail.com>
2024-05-19 17:31:04 -0700

    base model update

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-
-"""The super class that others will inherit from"""
+"""The super class that others will inhirit from"""
 
 import uuid
 from datetime import datetime
 from models import storage
-
 
 class BaseModel:
     """Defines all attributes for the classes"""
@@ -19,28 +17,29 @@ class BaseModel:
             self.updated_at = self.created_at
             storage.new(self)
         else:
-            format = "%Y-%m-%d T%H:%M:%S.%f"
+            format = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key in {'created_at', 'udated_at'}:
+                if key in {'created_at', 'updated_at'}:
                     value = datetime.strptime(value, format)
                 if key != '__class__':
                     setattr(self, key, value)
-	def __str__(self)
-		"""print class name id and attributes of the dictionary"""
-		class_name = self.__class__.__name__
-		return "[{}] [{}] [{}]" .format(class_name, self.id, sel.__dict__)
 
-	def save(self)
-		"""updates the attribute updated_at to the current time"""
-		self.updated_at = datetime.now()
-		storage.save()
+    def __str__(self):
+       """Print class name id and attribute dictionary"""
+       class_name = self.__class__.__name__
+       return "[{}] [{}] [{}]"  .format(class_name, self.id, self.__dict__)  
 
-	def to_dict(self)
-		"""create a dictionary representaion of the base model instances
-		convert datetime attributes to ISO format
-		"""
-		result = self.__dict__.copy()
-		result['crated_at'] = self.created_at.isoformat()
-		result['updated_at'] = self.updated_at.isoformat()
-		result['__class__'] = self.__class__.__name__
-		return result
+    def save(self):
+        """updates the attribute updated_at to the current time"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Create a dictionary representation of th base model instances
+        Convert datetime attributes to ISO format
+        """   
+        result = self.__dict__.copy()
+        result['created_at'] = self.created_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
+        result['__class__'] = self.__class__.__name__
+        return result
